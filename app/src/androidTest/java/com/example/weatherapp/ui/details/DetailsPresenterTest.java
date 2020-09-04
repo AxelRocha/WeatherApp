d7c063a3c0201ffa26c9bafbe3269c4b64db2276
AxelRocha <oaoexoufpr@gmail.com>
2020-09-04 04:21:06 -0300

    adding more tests to DetailsPresenter

--- app/src/androidTest/java/com/example/weatherapp/ui/details/DetailsPresenterTest.java
+++ app/src/androidTest/java/com/example/weatherapp/ui/details/DetailsPresenterTest.java
@@ -1,10 +1,12 @@
 package com.example.weatherapp.ui.details;
 
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Calendar;
 
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.times;
@@ -19,6 +21,7 @@ public class DetailsPresenterTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
+
 
     @Test
     public void redoSearch() {
@@ -49,4 +52,83 @@ public class DetailsPresenterTest {
         verify(view, times(1)).setForecastWeatherIcon(anyInt());
     }
 
+    @Test
+    public void getSunnyIcon() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        int icon = 0;
+
+        icon = presenter.getSunnyIcon(800, true);
+
+        Assert.assertNotEquals(0,icon);
+    }
+
+    @Test
+    public void getCloudyIcon() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        int icon = 0;
+
+        icon = presenter.getCloudyIcon(700, false);
+
+        Assert.assertNotEquals(0,icon);
+    }
+
+    @Test
+    public void getSnowIcon() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        int icon = 0;
+
+        icon = presenter.getSnowIcon(600, false);
+
+        Assert.assertNotEquals(0,icon);
+    }
+
+    @Test
+    public void getRainIcon() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        int icon = 0;
+
+        icon = presenter.getRainIcon(500, false);
+
+        Assert.assertNotEquals(0,icon);
+    }
+
+    @Test
+    public void getDrizzleIcon() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        int icon = 0;
+
+        icon = presenter.getDrizzleIcon(300,false);
+
+        Assert.assertNotEquals(0,icon);
+    }
+
+
+    @Test
+    public void getThunderIcon() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        int icon = 0;
+
+        icon = presenter.getThunderIcon(200,false);
+
+        Assert.assertNotEquals(0,icon);
+    }
+
+    @Test
+    public void isSunUp() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+        boolean isSunNow = false;
+        boolean isSunTest;
+        int currentHour;
+
+        Calendar rightNow = Calendar.getInstance();
+        currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
+
+        if (currentHour > 6 && currentHour < 19){
+            isSunNow = true;
+        }
+
+        isSunTest = presenter.isSunUp();
+
+        Assert.assertEquals(isSunNow, isSunTest);
+    }
 }
