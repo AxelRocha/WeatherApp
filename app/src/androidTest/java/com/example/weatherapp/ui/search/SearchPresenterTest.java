beec278ea9b41ea0d3e28442dd7ae6841bf32a77
AxelRocha <oaoexoufpr@gmail.com>
2020-09-07 13:27:13 -0300

    change of tests, resulting from the presenters change of responsibility

--- app/src/androidTest/java/com/example/weatherapp/ui/search/SearchPresenterTest.java
+++ app/src/androidTest/java/com/example/weatherapp/ui/search/SearchPresenterTest.java
@@ -1,17 +1,23 @@
 package com.example.weatherapp.ui.search;
 
+import com.example.weatherapp.model.WeatherAssets;
 import com.example.weatherapp.model.requestResponses.currentWeather.CurrentWeatherRequestResponse;
 import com.example.weatherapp.model.requestResponses.forecastWeather.ForecastWeatherRequestResponse;
 import com.example.weatherapp.network.GetDataService;
+import com.example.weatherapp.ui.details.DetailsPresenter;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Calendar;
+
 import retrofit2.Call;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -60,5 +66,97 @@ public class SearchPresenterTest {
         presenter.searchWeather("", retrofit);
 
         verify(retrofit, times(0)).getCurrentWeather("");
+    }
+
+    @Test
+    public void getSunnyIcon() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        WeatherAssets weatherAssets;
+
+        weatherAssets = presenter.getSunnyAssets(800, true);
+
+        Assert.assertNotNull(weatherAssets);
+        Assert.assertNotEquals(0,weatherAssets.getWeatherIcon());
+        Assert.assertNotEquals(0,weatherAssets.getWeatherBackground());
+    }
+
+    @Test
+    public void getCloudyIcon() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        WeatherAssets weatherAssets;
+
+        weatherAssets = presenter.getSunnyAssets(800, true);
+
+        Assert.assertNotNull(weatherAssets);
+        Assert.assertNotEquals(0,weatherAssets.getWeatherIcon());
+        Assert.assertNotEquals(0,weatherAssets.getWeatherBackground());
+    }
+
+    @Test
+    public void getSnowIcon() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        WeatherAssets weatherAssets;
+
+        weatherAssets = presenter.getSunnyAssets(600, true);
+
+        Assert.assertNotNull(weatherAssets);
+        Assert.assertNotEquals(0,weatherAssets.getWeatherIcon());
+        Assert.assertNotEquals(0,weatherAssets.getWeatherBackground());
+    }
+
+    @Test
+    public void getRainIcon() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        WeatherAssets weatherAssets;
+
+        weatherAssets = presenter.getSunnyAssets(500, true);
+
+        Assert.assertNotNull(weatherAssets);
+        Assert.assertNotEquals(0,weatherAssets.getWeatherIcon());
+        Assert.assertNotEquals(0,weatherAssets.getWeatherBackground());
+    }
+
+    @Test
+    public void getDrizzleIcon() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        WeatherAssets weatherAssets;
+
+        weatherAssets = presenter.getSunnyAssets(300, true);
+
+        Assert.assertNotNull(weatherAssets);
+        Assert.assertNotEquals(0,weatherAssets.getWeatherIcon());
+        Assert.assertNotEquals(0,weatherAssets.getWeatherBackground());
+    }
+
+
+    @Test
+    public void getThunderIcon() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        WeatherAssets weatherAssets;
+
+        weatherAssets = presenter.getSunnyAssets(200, true);
+
+        Assert.assertNotNull(weatherAssets);
+        Assert.assertNotEquals(0,weatherAssets.getWeatherIcon());
+        Assert.assertNotEquals(0,weatherAssets.getWeatherBackground());
+    }
+
+    @Test
+    public void isSunUp() {
+        SearchPresenter presenter = new SearchPresenter(view);
+        boolean isSunNow = false;
+        boolean isSunTest;
+        int currentHour;
+
+        Calendar rightNow = Calendar.getInstance();
+        currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
+
+        if (currentHour > 6 && currentHour < 19){
+            isSunNow = true;
+        }
+
+        isSunTest = presenter.isSunUp();
+
+        Assert.assertEquals(isSunNow, isSunTest);
     }
 }
