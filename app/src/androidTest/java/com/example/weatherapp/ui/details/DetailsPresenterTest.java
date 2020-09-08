f5febc6a16fbc5945f867845007d801ceb09d1c6
AxelRocha <oaoexoufpr@gmail.com>
2020-09-08 16:33:27 -0300

    creating tests of details presenter

--- app/src/androidTest/java/com/example/weatherapp/ui/details/DetailsPresenterTest.java
+++ app/src/androidTest/java/com/example/weatherapp/ui/details/DetailsPresenterTest.java
@@ -1,5 +1,7 @@
 package com.example.weatherapp.ui.details;
 
+import com.example.weatherapp.model.CurrentWeather;
+import com.example.weatherapp.model.ForecastWeather;
 import com.example.weatherapp.model.WeatherAssets;
 
 import org.junit.Assert;
@@ -19,6 +21,15 @@ public class DetailsPresenterTest {
     @Mock
     private DetailsContract.View view;
 
+    @Mock
+    private CurrentWeather currentWeather;
+
+    @Mock
+    private ForecastWeather todayForecastWeather;
+
+    @Mock
+    private ForecastWeather tomorrowForecastWeather;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -32,5 +43,23 @@ public class DetailsPresenterTest {
         presenter.redoSearch();
 
         verify(view, times(1)).returnToSearchActivity();
+    }
+
+    @Test
+    public void populateCurrentWeatherInformations() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+
+        presenter.populateWeatherInformations(currentWeather, todayForecastWeather, tomorrowForecastWeather);
+
+        verify(view, times(1)).populateCurrentWeatherInformation(currentWeather, todayForecastWeather);
+    }
+
+    @Test
+    public void populateForecastWeatherInformations() {
+        DetailsPresenter presenter = new DetailsPresenter(view);
+
+        presenter.populateWeatherInformations(currentWeather, todayForecastWeather, tomorrowForecastWeather);
+
+        verify(view, times(1)).populateForecastWeatherInformation(tomorrowForecastWeather);
     }
 }
